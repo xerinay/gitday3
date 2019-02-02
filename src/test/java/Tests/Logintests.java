4e581fb4e1a917cee736144a778fe9d13e28c861
Shernay <xerinay85@gmail.com>
2019-02-01 21:28:18 -0500

    User name has been adjusted

--- src/test/java/Tests/Logintests.java
+++ src/test/java/Tests/Logintests.java
@@ -23,7 +23,7 @@ public class Logintests {
     @Test
     public void logintest1(){
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Testers");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
         Assert.assertEquals(driver.getTitle(),"Web Orders");
     }
